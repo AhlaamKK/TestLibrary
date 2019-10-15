07f91c2d91f0c95eb0f9dface0e88338bca506fc
AhlaamKK <ahlaam.kazi@gmail.com>
2019-10-16 00:33:09 +0300

    commit before push

--- mygoselllibrary/src/main/java/com/example/mygoselllibrary/ApiConnection/ApiInterfaces.java
+++ mygoselllibrary/src/main/java/com/example/mygoselllibrary/ApiConnection/ApiInterfaces.java
@@ -6,6 +6,7 @@ import com.example.mygoselllibrary.models.UserResponseParser;
 
 import retrofit2.Call;
 import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
 
@@ -14,12 +15,12 @@ public interface ApiInterfaces {
 
         @GET("1")
          Call<UserResponseParser> getUserDetails();
-
+        @FormUrlEncoded
         @POST("credits")
-         Call<CreditResponseParser> getCreditTransactions(@Field("id") String Id, @Field("currency") String currency, @Field("amount") String amount, @Field("source") String sourceId);
+         Call<CreditResponseParser> getCreditTransactions(@Field("id") String Id, @Field("currency") String currency, @Field("amount") String amount, @Field("source") Object sourceId);
 
         @POST("debits")
-        Call<CreditResponseParser> getDebitTransactions(@Field("id") String Id, @Field("currency") String currency, @Field("amount") String amount, @Field("destination_id") String destinationId, @Field("source") String sourceId);
+        Call<CreditResponseParser> getDebitTransactions(@Field("id") String Id, @Field("currency") String currency, @Field("amount") String amount, @Field("destination_id") String destinationId, @Field("source") Object sourceId);
 
 
 
